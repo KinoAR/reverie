2e4a8eeb93d354f57baa45544d7423c2a2ff501b
Kino  Rose <kinoarose@gmail.com>
2020-06-03 21:28:01 -0400

    Implement color utils class

--- src/rev/Text.hx
+++ src/rev/Text.hx
@@ -1,23 +1,41 @@
 package rev;
 import math.V2;
+import kha.Font;
+
 class Text extends Drawable {
 
   public var letterSpacing:Int;
   public var lineSpacing:Int;
   public var maxWidth:Float;
+  public var fontSize:Int;
+  public var font:kha.Font;
   public var textColor:Int;
-  public var textHeight:Int;
-  public var textWidth:Int;
+  public var textHeight (get, null): Float;
+  public var textWidth (get, null) :Float;
   public var text:String;
+  // public var text (get,set):String;
 
   public function new(rc:RenderContext, font:String, ?parent:Object) {
     super(rc, parent);
     this.textColor = 0xFFFFFF;
+    this.text = "";
+  }
+
+  private function get_textHeight():Float {
+    return this.font.height(this.fontSize);
+  }
+
+  private function get_textWidth():Float {
+    return this.font.width(this.fontSize, this.text);
   }
 
   public function setText(text:String) {
     this.text = text;
   }
+
+  public function getText() {
+    return this.text;
+  } 
 
   public function drawText(text:String, position:V2) {
     this.setText(text);
